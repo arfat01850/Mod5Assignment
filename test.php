de1588a1acf5d8b257487e9b1a8e0bd355dc6a50
arfat01850 <arfat01850@gmail.com>
2023-03-03 00:36:52 +0600

    a

--- test.php
+++ test.php
@@ -1,7 +1,22 @@
 
 <?php
-$array1 = array("color" => "red", 2, 4);
-// $array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
-$result = array_merge($array1);
-print_r($result);
-?>
+
+
+// $array1 = array("color" => "red", 2, 4);
+// // $array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
+// $result = array_merge($array1);
+// print_r($result);
+
+
+// $array = array(0,1,2,3,4,5,6,7,8,9);
+// $value = array_search();
+// print_r($value);
+
+
+$queue = [
+    "orange",
+    "banana"
+];
+
+array_unshift($queue, "apple", "raspberry");
+var_dump($queue);
